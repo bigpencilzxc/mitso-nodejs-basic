d00518f29ad8e5d7e82f71bbbff52dee245c0ca4
bigpencilzxc <maksimmatiukhin05@gmail.com>
2023-03-13 15:49:50 +0300

    feat: decompress task completed

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path"
+import * as url from "url"
+import { createUnzip} from "zlib";
+
+const _dirname = url.fileURLToPath(new URL(".", import.meta.url))
+const filesPath = path.join(_dirname, "archive.gz")
+
 const decompress = async () => {
-    // Write your code here 
+    const input = createReadStream(filesPath)
+    const zip = createUnzip()
+    const write = createWriteStream(path.join(_dirname,"fileToCompress.txt"), {
+        encoding: "utf-8"
+    })
+
+    try {
+        input.pipe(zip).pipe(write)
+        console.log("file unzipped")
+    } catch (err) {
+        console.log(err.message)
+    }
 };
 
 await decompress();
