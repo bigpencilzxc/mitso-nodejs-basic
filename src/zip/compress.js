e3c0ba52b18c392c06a4da008d3bff5bef927092
bigpencilzxc <maksimmatiukhin05@gmail.com>
2023-03-13 15:49:39 +0300

    feat: compress task completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "fs";
+import path from "path"
+import { pipeline } from "stream";
+import * as url from "url"
+import { createGzip} from "zlib";
+
+const _dirname = url.fileURLToPath(new URL(".", import.meta.url))
+const filesPath = path.join(_dirname, "files", "fileToCompress.txt")
+
 const compress = async () => {
-    // Write your code here 
+    const input = createReadStream(filesPath, {
+        encoding: "utf-8",
+    })
+    const zip = createGzip()
+    const write = createWriteStream(path.join(_dirname, "archive.gz"));
+
+    try {
+        input.pipe(zip).pipe(write)
+        console.log("file zipped")
+    } catch (err) {
+        console.log(err.message)
+    }
 };
 
 await compress();
