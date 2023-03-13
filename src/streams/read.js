1d0cf3aac5dcc28a96d4220deb3ac938e478d39f
bigpencilzxc <maksimmatiukhin05@gmail.com>
2023-03-13 15:55:57 +0300

    feat: read task completed

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import { createReadStream } from "fs";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+import { Readable } from "stream";
+import path from "path";
+
+const dirname = fileURLToPath(new URL(".", import.meta.url));
+const fileToRead = path.join(dirname, "files", "fileToRead.txt");
+
+class MyRead extends Read {
+    constructor(opr) {
+        super(opr);
+    }
+
+    _read() {
+        const input = createReadStream(fileToRead)
+        input.on("data", (chunk) => {
+          return chunk;
+        })
+
+    }
+}
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = new MyReadable();
+    stdout.pipe(readableStream);
 };
 
 await read();
